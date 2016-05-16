9016e1c6339a4ba8b9bd06ec2cf33602d84268b3
Asher <asherehsa@gmail.com>
2016-05-15 17:16:30 -0700

    js fix

--- js/script.js
+++ js/script.js
@@ -9,15 +9,12 @@ $(document).ready(function(){
         var hours = date.getHours();
         var minutes = date.getMinutes();
         var seconds = date.getSeconds();
-        return date;
         var arr = [hours, minutes, seconds].map(function(num){
             //pad the numbers and make them strings
             return num < 10 ? '0' + num: String(num);
-        })
+        });
         //make it into hex
-        var color = '#' + arr[0] + arr[1] + ar[2];
-
-        document.getElementById('about').style.color = color;
+        document.getElementById('about').style.color = '#' + arr[0] + arr[1] + ar[2];
     }
 });
 
