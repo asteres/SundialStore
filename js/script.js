d3bf1c2f0a5b1926e7620ea9f762544a293b07bc
Asher <asherehsa@gmail.com>
2016-05-18 22:55:52 -0700

    other

--- js/script.js
+++ js/script.js
@@ -55,7 +55,7 @@ $(document).ready(function () {
 
     //show textbox when other is selected and focus the textbox
     $('#options').click(function() {
-  $('#amount-custom')[this.checked ? "show" : "hide"]();
+  $('#amount-custom').slideToggle(500);
 });
 
 });
